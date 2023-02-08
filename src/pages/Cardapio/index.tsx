484b1120a9b67417e43c88685fd7186d12c08eaa
Leonardo <leonardorodrigues@grendene.com.br>
2023-02-08 17:41:27 -0300

    feact: Adiciona caminho absoluto usando urlbase

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,11 +1,11 @@
-import styles from "./Cardapio.module.scss";
-import logo from "../../assets/logo.svg";
+ import styles from "./Cardapio.module.scss";
+import {ReactComponent as Logo} from "assets/logo.svg";
 
 export default function Cardapio() {
   return (
     <main>
       <nav className={styles.menu}>
-        <img src={logo} alt="logo do aluroni" />
+        <Logo/>
       </nav>
 
     </main>
