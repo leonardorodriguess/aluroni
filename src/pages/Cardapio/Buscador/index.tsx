b7f44ff4aafa0b6da4d400f7a9cf6be407f9ae61
Leonardo Rodrigues <leo-feitosa@live.com>
2023-02-25 18:16:29 -0300

    feact: Cria componente de busca

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,22 @@
+import styles from './Buscador.module.scss';
+import { CgSearch } from 'react-icons/cg';
+
+interface IBuscador {
+  busca: string,
+  setBusca: React.Dispatch<React.SetStateAction<string>>,
+}
+
+export default function Buscador ({busca, setBusca} : IBuscador) {
+  return(
+    <div className={styles.buscador}>
+      <input
+        value={busca}
+        onChange={e => setBusca(e.target.value)}
+      />
+      <CgSearch 
+        size={20}
+        color="#4C4D5E"
+      />
+    </div>
+  );
+}
