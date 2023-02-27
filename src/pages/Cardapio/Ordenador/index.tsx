d57c791877f863e8f0d39afca56ffc7177e47065
Leonardo Rodrigues <leo-feitosa@live.com>
2023-02-27 16:01:08 -0300

    style: Ajusta visualização ordenador

--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -1,13 +1,30 @@
 import styles from './Ordenador.module.scss';
 import opcoes from './opcoes.json';
 
-export default function Ordenador () {
+import { useState } from 'react';
+import classNames from 'classnames';
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
+
+export default function Ordenador() {
+
+  const [aberto, setAberto] = useState(false);
+
   return (
-    <button className={styles.ordenador}>
+    <button 
+      className={styles.ordenador}
+      onClick={() => setAberto(!aberto)}
+      onBlur={() => setAberto(false)}
+    >
       <span>
         Ordenar Por
       </span>
-      <div className={styles.ordenador__options}>
+
+      {aberto ? < MdKeyboardArrowUp size={20}/> : <MdKeyboardArrowDown size={20}/>}
+
+      <div className={classNames({
+        [styles.ordenador__options]: true,
+        [styles["ordenador__options--ativo"]]: aberto,
+      })}>
         {opcoes.map(opcao => (
           <div className={styles.ordenador__option} key={opcao.value}>
             {opcao.nome}
