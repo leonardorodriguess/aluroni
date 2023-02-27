58c93ef56ef440fe7e4ef76ebc0f5ce238fde2e9
Leonardo <leo-feitosa@live.com>
2023-02-27 17:52:28 -0300

    feact: Atualiza tipos

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,5 +1,6 @@
 import styles from './Item.module.scss';
 import cardapio from '../itens.json';
+import classNames from 'classnames';
 
 type Props = typeof cardapio[0];
 
@@ -26,7 +27,10 @@ export default function Item (props : Props) {
         </div>
         <div className={styles.item__tags}>
 
-          <div className={styles.item__tipo}>
+          <div className={classNames({
+            [styles.item__tipo]: true,
+            [styles[`item__tipo__${category.label.toLocaleLowerCase()}`]]: true
+          })}>
             {category.label}
           </div>
           <div className={styles.item__porcao}>
