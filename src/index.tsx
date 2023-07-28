7ba2e3666106a554fd77486557793e17352340dc
Leonardo <leo-feitosa@live.com>
2023-07-28 11:26:40 -0300

    feact: recomenda prato

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Cardapio from './pages/Cardapio';
+import Inicio from 'pages/Cardapio/Inicio';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <Cardapio/>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Inicio/>
+  </React.StrictMode>
 );
 
