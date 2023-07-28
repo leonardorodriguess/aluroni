0edc3d40898fcd8b8852ecb9efc6a687d51da341
Leonardo <leo-feitosa@live.com>
2023-07-28 16:19:34 -0300

    feact: Cria componente menu

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,5 +1,5 @@
 import styles from './Cardapio.module.scss';
-import { ReactComponent as Logo } from 'assets/logo.svg';
+
 import Buscador from './Buscador';
 import { useState } from 'react';
 import Filtros from './Filtros';
@@ -13,9 +13,7 @@ export default function Cardapio() {
 
   return (
     <main>
-      <nav className={styles.menu}>
-        <Logo />
-      </nav>
+      
 
       <header className={styles.header}>
         <div className={styles.header__text}>
